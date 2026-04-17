4eec4e2336f62c3a04500899409723f58747b739
Leonardo Fonteina <leofonteina06@gmail.com>
2026-04-17 17:08:56 -0300

    Le agregue un slider de productos abajo, tengo que modificarle para que quede lindo nomas

--- resources/views/frontend/Nexcell.blade.php
+++ resources/views/frontend/Nexcell.blade.php
@@ -139,7 +139,7 @@
 </div>
 
     <!-- 🔥 ASIDE ABAJO -->
-<aside class="row mt-4">
+    <aside class="row mt-4">
 
         <div class="col-md-4">
             <div class="card-custom">
@@ -164,6 +164,71 @@
 
     </aside>
 
+<div class="mt-5">
+    <h2 class="mb-4">Nuestros productos mas vendidos</h2>
+
+    <div id="carouselProductosMasVendidos" class="carousel slide" data-bs-ride="carousel">
+        
+        <div class="carousel-inner">
+
+            <!-- SLIDE 1 -->
+            <div class="carousel-item active">
+                <div class="d-flex justify-content-between">
+
+                    <div class="productosMasVendidos-card">
+                        <img src="{{ asset('images/Celulares/Apple/Gama Alta/iphone15ProMaxTitanio.jpg') }}">
+                        <p>Iphone 15 Pro Max</p>
+                        <strong>$2.176.900</strong>
+                    </div>
+
+                    <div class="productosMasVendidos-card">
+                        <img src="{{ asset('images/s25.png') }}">
+                        <p>Samsung S25</p>
+                        <strong>$1.771.900</strong>
+                    </div>
+
+                    <div class="productosMasVendidos-card">
+                        <img src="{{ asset('images/moto.png') }}">
+                        <p>Motorola G54</p>
+                        <strong>$526.500</strong>
+                    </div>
+
+                    <div class="productosMasVendidos-card">
+                        <img src="{{ asset('images/s25ultra.png') }}">
+                        <p>Samsung S25 Ultra</p>
+                        <strong>$2.316.100</strong>
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- SLIDE 2 -->
+            <div class="carousel-item">
+                <div class="d-flex justify-content-between">
+
+                    <div class="productosMasVendidos-card">
+                        <img src="{{ asset('images/moto2.png') }}">
+                        <p>Motorola G85</p>
+                        <strong>$671.300</strong>
+                    </div>
+
+                    <!-- agregás más productos -->
+                </div>
+            </div>
+
+        </div>
+
+        <!-- FLECHAS -->
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselProductosMasVendidos" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+        </button>
+
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselProductosMasVendidos" data-bs-slide="next">
+            <span class="carousel-control-next-icon"></span>
+        </button>
+
+    </div>
+</div>
 <!-- FOOTER -->
 
 
