64ffdff11930ba2faff924f54c9df34b20b4c945
k1ngao <agustin552689@gmail.com>
2026-04-26 18:57:04 -0300

    Cambios en comercializacion

--- resources/views/frontend/comercializacion.blade.php
+++ resources/views/frontend/comercializacion.blade.php
@@ -15,22 +15,51 @@
             <div class="col-md-4">
                 <div class="card-info">
                     <i class="bi bi-shop icono-card"></i>
+
                     <h4>Retiro en local</h4>
-                    <p>Podés retirar tu compra personalmente sin costo adicional.</p>
+
+                    <p>
+                        Podés retirar tu compra personalmente sin costo adicional.
+                    </p>
+
+                    <div class="logo-mini mt-3">
+                        <img src="/images/local.png" alt="Retiro en local">
+                    </div>
                 </div>
             </div>
 
             <div class="col-md-4">
                 <div class="card-info">
+
+                    <i class="bi bi-truck icono-card"></i>
+
                     <h4>Envío estándar</h4>
-                    <p>Entrega a domicilio en un plazo de 3 a 7 días hábiles en todo el país.</p>
+
+                    <p>
+                        Entrega a domicilio en un plazo de 3 a 7 días hábiles en todo el país.
+                    </p>
+
+                    <div class="logos-mini mt-3">
+                        <img src="/images/oca.png" alt="OCA">
+                        <img src="/images/correoargentino.png" alt="Correo Argentino">
+                    </div>
                 </div>
             </div>
 
             <div class="col-md-4">
                 <div class="card-info">
+
+                    <i class="bi bi-lightning-charge icono-card"></i>
+
                     <h4>Envío express</h4>
-                    <p>Recibí tu producto en 24 a 48 hs en zonas seleccionadas.</p>
+
+                    <p>
+                        Recibí tu producto en 24 a 48 hs en zonas seleccionadas.
+                    </p>
+
+                    <div class="logos-mini mt-3">
+                        <img src="/images/andreani.png" alt="Andreani">
+                    </div>
                 </div>
             </div>
 
@@ -47,15 +76,33 @@
 
             <div class="col-md-6">
                 <div class="card-info">
+
                     <h4>Correo privado</h4>
-                    <p>Trabajamos con empresas de logística reconocidas para garantizar rapidez y seguridad.</p>
+
+                    <p>
+                        Trabajamos con empresas de logística reconocidas para garantizar rapidez y seguridad.
+                    </p>
+
+                    <div class="logos-mini mt-3">
+                        <img src="/images/Comercializacion/Correo_oca_logo.png" alt="OCA">
+                        <img src="/images/Comercializacion/Logo_de_Andreani.jpg" alt="Andreani">
+                        <img src="/images/Comercializacion/correoArgentino.png" alt="Correo Argentino">
+                    </div>
                 </div>
             </div>
 
             <div class="col-md-6">
                 <div class="card-info">
+
                     <h4>Seguimiento del pedido</h4>
-                    <p>Recibirás un código de seguimiento para saber en todo momento dónde está tu compra.</p>
+
+                    <p>
+                        Recibirás un código de seguimiento para saber en todo momento dónde está tu compra.
+                    </p>
+
+                    <div class="logo-mini mt-3">
+                        <img src="/images/tracking.png" alt="Seguimiento">
+                    </div>
                 </div>
             </div>
 
@@ -68,27 +115,52 @@
             <i class="bi bi-credit-card icono-seccion"></i> Formas de Pago
         </h2>
 
-
         <div class="row g-4 mt-3">
 
             <div class="col-md-4">
                 <div class="card-info">
+
                     <h4>Tarjetas</h4>
-                    <p>Aceptamos tarjetas de crédito y débito. Posibilidad de cuotas según promociones.</p>
+
+                    <p>
+                        Aceptamos tarjetas de crédito y débito. Posibilidad de cuotas según promociones.
+                    </p>
+
+                    <div class="logos-mini mt-3">
+                        <img src="/images/Comercializacion/visaTarjeta.png" alt="Visa">
+                        <img src="/images/Comercializacion/Mastercard.png" alt="Mastercard">
+                        <img src="/images/Comercializacion/naranjaXtarj.png" alt="Naranja X">
+                    </div>
                 </div>
             </div>
 
             <div class="col-md-4">
                 <div class="card-info">
+
                     <h4>Transferencia</h4>
-                    <p>Pagá mediante transferencia bancaria con descuentos exclusivos.</p>
+
+                    <p>
+                        Pagá mediante transferencia bancaria con descuentos exclusivos.
+                    </p>
+
+                    <div class="logos-mini mt-3">
+                        <img src="/images/mercadopago.png" alt="Mercado Pago">
+                    </div>
                 </div>
             </div>
 
             <div class="col-md-4">
                 <div class="card-info">
+
                     <h4>Efectivo</h4>
-                    <p>Podés abonar en efectivo al retirar tu producto en el local.</p>
+
+                    <p>
+                        Podés abonar en efectivo al retirar tu producto en el local.
+                    </p>
+
+                    <div class="logo-mini mt-3">
+                        <img src="/images/efectivo.png" alt="Efectivo">
+                    </div>
                 </div>
             </div>
 
